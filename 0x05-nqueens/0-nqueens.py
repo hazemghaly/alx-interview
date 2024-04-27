c44b26160d7cc5cbe78ae589360de3e1370e2005
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-04-27 04:18:25 +0300

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,17 +1,51 @@
 #!/usr/bin/python3
+"""interview """
+
 
 import sys
 
+
+def is_available(board, row, col, N):
+    """check availablty of this board"""
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+    return True
+
+
+def solve(board, col, N, result):
+    """Solve Nqueens with recursive """
+    if col >= N:
+        v = []
+        for i in range(N):
+            for j in range(N):
+                if board[i][j] == 1:
+                    v.append([i, j])
+        result.append(v)
+        return True
+
+    res = False
+    for i in range(N):
+        if is_available(board, i, col, N):
+            board[i][col] = 1
+            res = solve(board, col + 1, N, result) or res
+            board[i][col] = 0  # Backtrack
+
+    return res
+
+
 def queens(N):
-    """Nqueens"""
-    result = []
-    N = int(sys.argv[1])
-    board = [[0] * N for _ in range(N)]
-    stack = []  # Stack to simulate recursive calls
-    row, col = 0, 0
+    """check cmd arguments"""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         exit(1)
+
     if not isinstance(N, int):
         print("N must be a number")
         exit(1)
@@ -19,22 +53,15 @@ def queens(N):
         print("N must be at least 4")
         exit(1)
 
-    if (col >= 4):
-        v = []
-        for i in board:
-          for j in range(len(i)):
-            if i[j] == 1:
-              v.append(j+1)
-        result.append(v)
-        return True
-    res = False
-    for i in range(4):
-        if (board, i, col, N):
-            board[i][col] = 1
-            res = queens(N) or res
-            board[i][col] = 0  # BACKTRACK
-    return res
+    board = [[0] * N for _ in range(N)]
+    result = []
+    if not solve(board, 0, N, result):
+        print("Solution does not exist")
+        return
+    for res in result:
+        print(res)
 
 
 if __name__ == "__main__":
-    queens(4)
+    N = int(sys.argv[1])
+    queens(N)
