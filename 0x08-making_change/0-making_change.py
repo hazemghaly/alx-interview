6894ac3d7709bf426284fc9ed4913e14568d4cfd
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-03 04:19:36 +0300

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -15,7 +15,7 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     for i in range(1, total + 1):
-        for j in range(m):
+        for j in range(n):
             if coins[j] <= i:
-                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+                dp[i] += dp[i - coins[j]]
+   return dp[total] if dp[total] else -1
