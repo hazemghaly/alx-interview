15344c3a15cd1dd5d3040beaeb0b34a7dacb8d80
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-03 04:34:11 +0300

    Add files via upload

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,17 +9,22 @@ def makeChange(coins, total):
     determine the fewest number of
     coins needed to meet a given amount total
     '''
+    # n = len(coins)
+    # dp = [0] * (total + 1)
+    # dp[0] = 1
+    # if total <= 0:
+    #     return 0
+    # for i in range(1, total + 1):
+    #     for j in range(n):
+    #         if coins[j] <= i:
+    #             dp[i] += dp[i - coins[j]]
+    # return dp[total] if dp[total] else -1
     n = len(coins)
-    dp = [0] * (total + 1)
-    dp[0] = 1
-    if total <= 0:
-        return 0
+    v = float('inf')
+    dp = [v] * (total + 1)
+    dp[0] = 0
     for i in range(1, total + 1):
         for j in range(n):
             if coins[j] <= i:
-                dp[i] += dp[i - coins[j]]
-    return dp[total] if dp[total] else -1
-
-    # else:
-    #     return min((makeChange(total - coin, coins) + [coin]
-    #                 for coin in coins if total >= coin), key=len)
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+    return dp[total] if dp[total] != v else -1
