6cdf26ddc53eb73180e38cc3b7911f9bb7934ea2
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-03 03:56:28 +0300

    Add files via upload

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""interview """
+
+
+def makeChange(coins, total):
+    '''
+    making change in coins
+    a pile of coins of different values
+    determine the fewest number of
+    coins needed to meet a given amount total
+    '''
+    n = len(coins)
+    dp = [0] * (total + 1)
+    dp[0] = 1
+    if total <= 0:
+        return 0
+    for i in range(n):
+        for j in range(coins[i], total + 1):
+            dp[j] += dp[j - coins[i]]
+
+    return dp[total] if dp[total] else -1
