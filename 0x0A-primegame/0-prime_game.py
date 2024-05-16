09f176715607d16d6ea7a52d4ea11b7a73ed84a3
Hazem Ghaly <125504512+hazemghaly@users.noreply.github.com>
2024-05-16 22:40:08 +0300

    Add files via upload

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''interview taskes
+'''
+
+
+def isWinner(x, nums):
+    ''' island prrimeter task interview
+'''
+    primes = []
+    if x < 1 or not nums:
+        return None
+    if x != len(nums):
+        return None
+    marwin, benwin = 0, 0
+    n = max(nums)
+    primes = [True for _ in range(1, n + 1, 1)]
+    primes[0] = False
+    for i, is_prime in enumerate(primes, 1):
+        if i == 1 or not is_prime:
+            continue
+        for j in range(i + i, n + 1, i):
+            primes[j - 1] = False
+    for i in nums:
+        if sum(primes[0:i + 1]) % 2 == 0:
+            marwin += 1
+        else:
+            benwin += 1
+    if marwin == benwin:
+        return None
+    if marwin > benwin:
+        return 'Maria'
+    else:
+        return 'Ben'
